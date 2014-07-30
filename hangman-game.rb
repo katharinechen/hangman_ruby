8cb77f84d804100c49b4aaad582b7e14a92723ab
Justin Speers & Katharine Chen <student@epicodus.com>
2014-07-30 14:49:07 -0700

    Update art work

--- hangman-game.rb
+++ hangman-game.rb
@@ -1,8 +1,17 @@
 require './lib/hangman'
 require './lib/word'
 
-def initialize
-  word_list = %w{ random hello hangman justin katharine }
+def initialize_game
+  word_list = %w{
+                  honorificabiltudinitatibus
+                  antidisestablishmentarianism
+                  floccinaucinihiliplication
+                  psuedopseudohypoparathyrodism
+                  pneumonoultramicroscopicsilicovolcanoconiosis
+                  aequeosalinocalcalinoceraceoaluminosocupreovitriolic
+                  lopadotemachoselachogaleokranioleipsanodrimhypotrimmatosilphioparaomelitokatakechymenokichlepikossyphophattoperisteralektryonoptekephalliokigklopeleiolagoiosiraiobaphetraganopterygon
+                  methionylglutaminylarginyltyrosylglutamylserylleucylphenylalanylalanylglutaminylleucyllysylglutamylarginyllysylglutamylglycylalanylphenylalanylvalylprolylphenylalanylvalylthreonylleucylglycylaspartylprolylglycylisoleucylglutamylglutaminylserylleucyllysylisoleucylaspartylthreonylleucylisoleucylglutamylalanylglycylalanylaspartylalanylleucylglutamylleucylglycylisoleucylprolylphenylalanylserylaspartylprolylleucylalanylaspartylglycylprolylthreonylisoleucylglutaminylaspfraginylalanylthreonylleucylarginylalanylphenylalanylalanylalanylglycylvalylthreonylprolylalanylglutaminylcysteinylphenylalanylglutamylmethionylleucylalanylleucylisoleucylarginylglutaminyllysylhistidylprolylthreonylisoleucylprolylisoleucylglycylleucylleucylmethionyltyrosylalanylasparaginylleucylvalylphenylalanylasparaginyllysylglycylisoleucylaspartylglutamylphenylalanyltyrosylalanylglutaminylcysteinylglutamyllysylvalylglycylvalylaspartylserylvalylleucylvalylalanylaspartylvalylprolylvalylglutaminylglutamylserylalanylprolylphenylalanylarginylglutaminylalanylalanylleucylarginylhistidylasparaginylvalylalanylprolylisoleucylphenylalanylisoleucylcysteinylprolylprolylaspartylalanylaspartylaspartylaspartylleucylleucylarginylglutaminylisoleucylalanylseryltyrosylglycylarginylglycyltyrosylthreonyltyrosylleucylleucylserylarginylalanylglycylvalylthreonylglycylalanylglutamylasparaginylarginylalanylalanylleucylprolylleucylasparaginylhistidylleucylvalylalanyllysylleucyllysylglutamyltyrosylasparaginylalanylalanylprolylprolylleucylglutaminylglycylphenylalanylglycylisoleucylserylalanylprolylaspartylglutaminylvalyllysylalanylalanylisoleucylaspartylalanylglycylalanylalanylglycylalanylisoleucylserylglycylserylalanylisoleucylvalyllysylisoleucylisoleucylglutamylglutaminylhistidylasparaginylisoleucylglutamylprolylglutamyllysylmethionylleucylalanylalanylleucyllysylvalylphenylalanylvalylglutaminylprolylmethionyllysylalanylalanylthreonylarginylserine
+     }
   @word = Word.new(word_list[rand(word_list.length)])
   @game = Hangman.new
 end
@@ -17,28 +26,45 @@ def draw_screen
   puts "*" * 80
   puts "*" + " " * 35 + "Hangman!" + " " * 35 + "*"
   puts "*" * 80
+  puts @game.image
   puts @word.word_so_far.split(//).join(' ')
 end
 
 def main_menu
   draw_screen
-  guess = @word.guess?(prompt("Enter a letter:"))
+
+  letter = prompt("Enter a letter:").downcase
+  if letter == nil
+    prompt "Please choose a letter!"
+    return true
+  end
+
+  guess = @word.guess?(letter)
 
   if guess && @word.solved?
     draw_screen
-    prompt "You win!"
+    if prompt("You Saved Him! Play again(Y/N)?").downcase == 'y'
+      initialize_game
+      return true
+    end
     return false
   end
 
   @game.bad_guess unless guess
+  draw_screen
+
   if @game.game_over?
-    prompt "LOSER!"
+    if prompt("You failed! He's dead! Play again(Y/N)?").downcase == 'y'
+      initialize_game
+      return true
+    end
     return false
   end
 
   return true
 end
 
-initialize
+initialize_game
 while main_menu
 end
+
